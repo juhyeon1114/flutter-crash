5fd6c2536a86fd570728e329880418335c16a3d6
unknown <juhyeon1114@gmail.com>
2020-12-25 12:16:51 +0900

    45분까지 들음

---
+++ lib/random_words.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return Divider();
+        
+        final index = item;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPairs[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPair Generator')),
+      body: _buildList()
+    );
+  }
+}
